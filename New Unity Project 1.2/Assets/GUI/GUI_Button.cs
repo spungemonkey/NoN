453170b099fa9f5b5aeeb5be8346f517927b3eca
Michele Tammaro <mtamma10@caledonian.ac.uk>
2014-03-16 17:53:12 +0000

    16/03
    Basic Narration. Single script for GUI Elements. More SFX in.
    
    Fix Sun, Fix Bird. Add dust to walk. Fix Desert Movement. Fix Narration.

---
+++ New Unity Project 1.2/Assets/GUI/GUI_Button.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUI_Button : MonoBehaviour {
+
+	public GameObject[] obj;
+	public Texture2D butImg;
+	public AudioClip ac;
+	public int butPos;
+
+	bool guiEnabled = true;
+
+	// Use this for initialization
+	void Start () {
+
+
+		for (int i=0; i < obj.Length; i++) 
+		{
+			obj[i].SetActive(false);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI(){
+
+		GUI.enabled = guiEnabled;
+
+		if (GUI.Button (new Rect (butPos, Screen.height - 75, butImg.width, butImg.height), butImg)){
+			for (int i=0; i < obj.Length; i++) 
+			{
+				obj[i].SetActive(true);
+			}
+			audio.clip = ac;
+			audio.Play ();
+			guiEnabled = false;
+		}
+	}
+}
---
+++ New Unity Project 1.2/Assets/GUI/GUI_Narration.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUI_Narration : MonoBehaviour {
+
+	public GameObject obj;
+	public GameObject obj2;
+
+	public AudioClip ac;
+	public AudioClip ac2;
+	public AudioClip ac3;
+
+	public Texture2D butImg = null;
+	bool enab = true;
+	int aClip = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (aClip);
+		if (obj == null) {
+						aClip = 1;
+				}
+		if (obj2 == null) {
+			aClip = 2;		
+		}
+
+
+
+	}
+
+	void OnGUI(){
+
+		if (GUI.Button (new Rect (15, 15, butImg.width, butImg.height), butImg) && !audio.isPlaying){
+			Debug.Log("Hello");
+
+			//if (audio.isPlaying == false)
+			//{
+
+				switch (aClip)
+				{
+				case 0:
+					audio.clip = ac;
+					audio.Play();
+					break;
+				case 1:
+					audio.clip=ac2;
+					audio.Play();
+					break;
+				case 2:
+					audio.clip=ac3;
+					audio.Play();
+					break;
+				default:
+					audio.clip=ac;
+					audio.Play();
+					break;
+				}
+			//}
+		}
+	}
+
+}
