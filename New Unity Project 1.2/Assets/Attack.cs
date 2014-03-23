6ce3bca2d2ffe099bb09509ef1d7f062bf65536d
Michele Tammaro <mtamma10@caledonian.ac.uk>
2014-03-23 23:22:39 +0000

    Changes p2 - 23/03

---
+++ New Unity Project 1.2/Assets/Attack.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attack : MonoBehaviour {
+
+	//public GameObject attackObject;
+	public string WayPointTagName = "Enemy";
+	public GameObject conWP;
+	public NavMeshAgent navMesh;
+
+	// Use this for initialization
+	void Start () {
+		navMesh = GetComponent<NavMeshAgent>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		StartCoroutine (attackEnemies ());
+	}
+
+
+	public IEnumerator attackEnemies(){
+		
+		//Collider[] colliders=Physics.OverlapSphere(transform.position,2.0f);
+		Collider[] colliders = Physics.OverlapSphere (transform.position, 3.0f);
+		
+		foreach (Collider c in colliders) {
+			if (c.gameObject.tag == WayPointTagName)
+			{
+				//transform.position += new Vector3(10.0f, 10.0f, 10.0f);
+				navMesh.destination = c.gameObject.transform.position;
+				yield return new WaitForSeconds(0.01f);
+				navMesh.destination = conWP.transform.position;
+				
+			}
+		}
+	}
+
+}
