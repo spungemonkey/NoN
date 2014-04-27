1d9891cafd4f003251acb7bec3fa4df84618c59d
Michele Tammaro <mtamma10@caledonian.ac.uk>
2014-04-28 00:01:14 +0100

    HONOURS DEMO - IGNORE
    New level to pick what level. Other scripts added for purpose of report.
    Ignore this

---
+++ New Unity Project 1.2/Assets/NewBehaviourScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public Texture2D butImg1;
+	public Texture2D butImg2;
+	public Texture2D butImg3;
+	public Texture2D butImg4;
+	public Texture2D butImg5;
+	float butPos1;
+	float butPos2;
+	float butPos3;
+	float butPos4;
+	float butPos5;
+	// Use this for initialization
+	void Start () {
+
+		butPos1 = 100;
+		butPos2 = butPos1 + 65;
+		butPos3 = butPos2 + 65;
+		butPos4 = butPos3 + 65;
+		butPos5 = butPos4 + 65;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI(){
+		GUI.Button (new Rect (butPos1, Screen.height / 2 + 30, butImg1.width, butImg1.height), butImg1);
+		GUI.Button (new Rect (butPos2, Screen.height / 2 + 30, butImg2.width, butImg2.height), butImg2);
+		GUI.Button (new Rect (butPos3, Screen.height / 2 + 30, butImg3.width, butImg3.height), butImg3);
+		GUI.Button (new Rect (butPos4, Screen.height / 2 + 30, butImg4.width, butImg4.height), butImg4);
+		GUI.Button (new Rect (butPos5, Screen.height / 2 + 30, butImg5.width, butImg5.height), butImg5);
+
+		}
+}
---
+++ New Unity Project 1.2/Assets/levelSelect.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class levelSelect : MonoBehaviour {
+
+	public string[] levels;
+	int pos = 100;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI(){
+		//GUI.Button (new Rect (butPos1, Screen.height / 2 + 30, butImg1.width, butImg1.height), butImg1);
+		if (GUI.Button (new Rect (Screen.width / 2, pos, 200, 75), "0. Introduction")) {
+			Application.LoadLevel(levels[0]);
+			}
+		if (GUI.Button (new Rect (Screen.width / 2, pos + 100, 200, 75), "1. Waterfront")) {
+			Application.LoadLevel(levels[1]);
+		}
+		if (GUI.Button (new Rect (Screen.width / 2, pos + 200, 200, 75), "3. Village")) {
+			Application.LoadLevel(levels[2]);
+		}
+		if (GUI.Button (new Rect (Screen.width / 2, pos + 300, 200, 75), "4. Desert")) {
+			Application.LoadLevel(levels[3]);
+		}
+		if (GUI.Button (new Rect (Screen.width / 2, pos + 400, 200, 75), "5. Waterfront")) {
+			Application.LoadLevel(levels[4]);
+		}
+		if (GUI.Button (new Rect (Screen.width / 2, pos + 500, 200, 75), "6. Village")) {
+			Application.LoadLevel(levels[5]);
+		}
+		if (GUI.Button (new Rect (Screen.width / 2, pos + 600, 200, 75), "7. Desert")) {
+			Application.LoadLevel(levels[6]);
+		}
+	}
+}
